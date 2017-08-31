908a3b6e32c8d71815e5ceca828e2fe50c82ecd7
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2017-08-31 13:48:46 -0400

    Initial Application

---
+++ SimpleMailer/Options.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMailer
+{
+    public class Options
+    {
+        public string Host;
+        public int Port;
+        public string User;
+        public string Password;
+        public string From;
+        public List<string> To;
+        public string Subject;
+        public string Body;
+        public List<string> Attachments;
+    }
+}
---
+++ SimpleMailer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net.Mail;
+using System.Net;
+
+namespace SimpleMailer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Check to see if the configuration file was supplied and does exist
+            if (args.Length == 0 || !File.Exists(args[0]))
+            {
+                Console.WriteLine("Missing configuration file");
+                Environment.Exit(1);
+            }
+
+            // Load the options
+            Options options = JsonConvert.DeserializeObject<Options>(File.ReadAllText(args[0]));
+            
+            // Create a smpt client
+            SmtpClient client = new SmtpClient(options.Host, options.Port);
+            client.Credentials = new NetworkCredential(options.User, options.Password);
+            client.EnableSsl = true;
+
+            // Create a new message
+            MailMessage message = new MailMessage();
+            message.Subject = options.Subject;
+            message.Body = options.Body;
+            message.From = new MailAddress(options.From);
+
+            // Add all the recipients
+            foreach (string to in options.To)
+            {
+                message.To.Add(to);
+            }
+
+            // Add attachements if applicable
+            foreach (string attachement in options.Attachments)
+            {
+                if (File.Exists(attachement))
+                {
+                    message.Attachments.Add(new Attachment(attachement));
+                }
+            }
+
+            // Send the message
+            client.Send(message);
+        }
+    }
+}
